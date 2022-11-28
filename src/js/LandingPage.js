0b57716d69d327926c4bfc775fd2fd29757ce0ff
Sairamasubash Muppalaneni <smuppa3@illinois.edu>
2022-11-28 17:05:08 -0600

    final

--- src/js/LandingPage.js
+++ src/js/LandingPage.js
@@ -1,6 +1,8 @@
 // Here we are importing all the required items.
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Eventology from './Logo_Banner.png';
+import './LandingPage.scss';
 
 // Here we are creating a react class called App.
 class LandingPage extends React.Component  
@@ -11,11 +13,103 @@ class LandingPage extends React.Component
     // Here we are returning the format of the List View.
     return (
         <div>
-            <div className="buttonContainerDiv">
-                <Link to={'/LoginPage'}>Login Page</Link>
-                <br></br>
-                <Link to={'/SignupPage'}>Signup Page</Link>
+
+          <div className="header">
+
+            <Link to={'/'}>
+
+              <img src={Eventology} className='headerImage' alt="HI"/>
+
+            </Link>
+
+          </div>
+
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <div className="buttonContainerDiv">
+
+            <div className="buttonContainerDivOne">
+
+              <img src={Eventology} className='buttonContainerDivImage' alt="HI"/>
+
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+
+              <div className="links">
+
+                <Link className="LoginPageLink" to={'/LoginPage'}>Login</Link>
+
+                <Link className="SignupPageLink" to={'/SignupPage'}>Signup</Link>
+
+              </div>
+
             </div>
+
+            <div className="styleDivOne">
+
+            </div>
+
+          </div>
+
+          <div className="featureOne">
+
+            <div className="styleDivTwo">
+
+            </div>
+
+            <div className="featureOneText">
+
+              <h3>Feature One</h3>
+
+              <p>See events from Ticketmaster, Stubhub, etc. All in one centralized map.</p>
+
+            </div>
+
+          </div>
+
+          <div className="featureTwo">
+
+            <div className="featureTwoText">
+
+              <h3>Feature Two</h3>
+
+              <p>Change your account information and event preferences at any time you desire.</p>
+
+            </div>
+
+            <div className="styleDivThree">
+
+            </div>
+
+          </div>
+
+          <div className="featureThree">
+
+            <div className="styleDivFour">
+
+            </div>
+
+            <div className="featureThreeText">
+
+              <h3>Feature Three</h3>
+
+              <p> See a record of the events you have clicked on, and a record of the venues you have reviewed.</p>
+
+            </div>
+
+          </div>
+
+          <div className="footer">
+
+            <p>Copy Right @2022</p>
+
+          </div>
+
         </div>
     );
   }
--- src/js/LoginPage.js
+++ src/js/LoginPage.js
@@ -1,9 +1,8 @@
 // Here we are importing all the required items.
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './Styles.scss';
-// import List from './List';
-// import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Eventology from './Logo_Banner.png';
+import './LoginPage.scss';
 
 
 
@@ -16,7 +15,30 @@ class LoginPage extends React.Component
     // Here we are returning the format of the List View.
     return (
       <div>
-            Login Page
+        {/* <div className="headerTwo">
+
+          <Link to={'/'}>
+
+            <img src={Eventology} className='headerImageTwo' alt="HI"/>
+
+          </Link>
+
+        </div>
+
+        <div className="login"> 
+
+          <h6>LOGIN</h6>
+
+          <p>Username:</p>
+
+          <input placeholder="Enter Your Username"/>
+
+          <p>Password:</p>
+
+          <input placeholder="Enter Your Password"/>
+        
+        </div> */}
+
       </div>
     );
   }
