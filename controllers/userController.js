9038b036c6b67b04caef9224d41909fe30d617d4
Praneeth Reddygari <praneeth@Praneeths-MacBook-Air-2.local>
2022-11-30 10:05:29 -0600

    Finish signin and signout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
-var message = require('../models/message');
+const secrets = require('../config/secrets');
+const message = require('../models/message');
 const User = require("../models/user");
 const {validationResult} = require('express-validator');
+const jsonwebtoken = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 const createUser = function(req, res) {
     const errors = validationResult(req);
@@ -14,8 +17,40 @@ const createUser = function(req, res) {
             return res.status(400).json(message.response("Email already exists", {}));
         }
 
-        return res.status(201).json(message.response("Created User", user));
+        const {_id, username, email} = user;
+        return res.status(201).json(message.response("Created User", {_id: _id, username: username, email: email}));
     });
+}
+
+const signin = async function(req, res) {
+    const {email: email, password: password} = req.body;
+    try {
+        var user = await User.findOne({email: email}).exec();
+        if (!user) {
+            return res.status(400).json(message.response("Email Not Found", {}));
+        }
+
+        if (user.authenticate(password)) {
+            const token = jsonwebtoken.sign({_id: user._id}, secrets.jwt_sign_phrase);
+            res.cookie('token', token, {expire: new Date() + 3});
+            const {_id, username} = user;
+            return res.status(200).json(message.response("User signed in", {token: token, user: {_id: _id, username: username, email: email}}));
+        } else {
+            return res.status(400).json(message.response("Email and password don't match", {}));
+        }
+
+    } catch (err) {
+        return res.status(500).json(message.response("Sign in Failed", {}));
+    }
+}
+
+const signout = function(req, res) {
+    try {
+        res.clearCookie("token");
+        return res.status(200).json(message.response("Signout Successful", {}));
+    } catch (err) {
+        return res.status(500).json(message.response("Signout Failed", {}));
+    }
 }
 
 
@@ -30,3 +65,5 @@ const replaceUser = function(req, res) {
 exports.createUser = createUser;
 exports.getUser = getUser;
 exports.replaceUser = replaceUser;
+exports.signin = signin;
+exports.signout = signout;
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,20 @@ var {check} = require('express-validator');
 
 module.exports = function (router) {
     var userController = require('../controllers/userController');
-    var userRoute = router.route('/users');
+    var signupRoute = router.route('/signup');
+    var signinRoute = router.route('/signin');
+    var signoutRoute = router.route('/signout');
     var specifiedUserRoute = router.route('/users/:username')
 
-    userRoute.post([
+    signupRoute.post([
         check("name", "Name cannot be empty").isLength({min: 1}),
         check("username", "Username cannot be empty").isLength({min: 1}),
         check("email", "Email should be valid").isEmail(),
         check("password", "Password must be at least 6 characters").isLength({min: 6})
     ], userController.createUser);
+
+    signinRoute.post(userController.signin);
+    signoutRoute.get(userController.signout);
     // specifiedUserRoute.get((req, res) => userController.getUser(req, res));
     // specifiedUserRoute.put((req, res) => userController.replaceUser(req, res));
 
