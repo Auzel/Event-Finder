c4a0cc383b150c4792a87143ac62f8f184d59f8c
Praneeth Reddygari <praneeth@Praneeths-MacBook-Air-2.local>
2022-12-08 11:45:19 -0600

    Add reviews to getUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,10 +67,9 @@ const getUser = async function(req, res) {
         if (!user) {
             return res.status(404).json(message.response("User Not Found", {}));
         } else {
-            return res.status(200).json(message.response("OK", {_id: user._id, name: user.name, username: user.username, email: user.email, eventPrefs: user.eventPrefs}));
+            return res.status(200).json(message.response("OK", {_id: user._id, name: user.name, username: user.username, email: user.email, eventPrefs: user.eventPrefs, reviews: user.reviews}));
         }
     } catch (err) {
-        console.log(err);
         return res.status(500).json(message.response("Get User Failed", {}));
     }
 }
