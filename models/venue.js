6ef77fb4b52c120ccb191189e4d8433acce1c2d7
Auzel <filey6@hotmail.com>
2022-12-08 20:49:18 -0600

    added to event and venue models

--- models/venue.js
+++ models/venue.js
@@ -3,8 +3,15 @@ var mongoose = require('mongoose');
 
 // Define our venue schema
 var VenueSchema = new mongoose.Schema({
-    name: {type: String, required: true}
-    // add more here
+    _id: {type: mongoose.Schema.Types.ObjectId, required: true, unique : true},
+    name: {type: String, required: true} ,
+    location: { 
+        longitude: {type: String, required: true},
+        latitude: {type: String, required: true},
+    } ,    
+    events: {type: [String], required: true},
+    reviews: {type: [String], required: true},
+    rawReviewTotal: {type: Number, default: 0},     
 });
 
 // Export the Mongoose model
